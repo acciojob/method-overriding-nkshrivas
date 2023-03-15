c813c4f8ba3f121c71bb49c1b76b07d852365131
nkshrivas <nk733095@gmail.com>
2023-03-15 12:21:53 +0530

    Overridden again2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,17 +1,7 @@
 package com.driver;
 
 public class Main {
-    static class A{
-        String meth(){
-            return "Invoking method from class A";
-        }
-    }
 
-    static class B extends A{
-        String meth(){
-            return "Method is overridden in Extendend class B";
-        }
-    }
 
     public static void main(String[] args) {
      A objA = new A();
@@ -25,3 +15,4 @@ public class Main {
     }
   
 }
+
